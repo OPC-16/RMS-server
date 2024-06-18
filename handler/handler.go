60403428029f6479c5163db6a721ac621aeecfe3
OPC-16 <chavan.p.omkar@gmail.com>
2024-06-18 21:21:46 +0530

    Define uploadResume route

--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,14 @@
 package handler
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/OPC-16/RMS-server/model"
@@ -87,6 +92,7 @@ func Login(c echo.Context) error {
    token := jwt.New(jwt.SigningMethodHS256)
    claims := token.Claims.(jwt.MapClaims)
    claims["email"] = user.Email
+   claims["usertype"] = user.UserType
    claims["exp"] = time.Now().Add(time.Hour * 24).Unix() // Token expires in 24 hours
 
    //sign the token with a secret
@@ -100,5 +106,79 @@ func Login(c echo.Context) error {
 }
 
 func UploadResume(c echo.Context) error {
+   apiEndPoint := "https://api.apilayer.com/resume_parser/upload"
+   apiKey, _ := os.LookupEnv("API_KEY")
+   fmt.Println(apiKey)
+   filePath := "/home/omkar/resume.pdf"
+   data, err := fetchAPIData(apiEndPoint, apiKey, filePath)
+   if err != nil {
+      return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+   }
 
+   //TEMP: return the data as json
+   return c.JSON(http.StatusOK, data)
+}
+
+func fetchAPIData(apiEndPoint, apiKey, filePath string) (map[string]interface{}, error) {
+   //open the file
+   file, err := os.Open(filePath)
+   if err != nil {
+      return nil, err
+   }
+   defer file.Close()
+
+   //create a buffer to hold the multipart form data
+   body := &bytes.Buffer{}
+   writer := multipart.NewWriter(body)
+
+   //create a form field for the file
+   part, err := writer.CreateFormFile("file", filepath.Base(filePath))
+   if err != nil {
+      return nil, err
+   }
+
+   //copy the file contents to the form field
+   _, err = io.Copy(part, file)
+   if err != nil {
+      return nil, err
+   }
+
+   //close the writer to finalize the multipart form data
+   err = writer.Close()
+   if err != nil {
+      return nil, err
+   }
+
+   //create a new request
+   req, err := http.NewRequest("POST", apiEndPoint, body)
+   if err != nil {
+      return nil, err
+   }
+
+   //add the api key to the request header
+   req.Header.Add("apikey", apiKey)
+   req.Header.Add("Content-Type", "application/octet-stream")
+
+   //make the http request
+   client := &http.Client{}
+   resp, err := client.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+
+   //read the response body
+   responseBody, err := io.ReadAll(resp.Body)
+   if err != nil {
+      return nil, err
+   }
+
+   //parse the json response
+   var result map[string]interface{}
+   err = json.Unmarshal(responseBody, &result)
+   if err != nil {
+      return nil, err
+   }
+
+   return result, nil
 }
