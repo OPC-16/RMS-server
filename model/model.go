c0f34102772a04620953639f706de3b2f379dd2a
OPC-16 <chavan.p.omkar@gmail.com>
2024-06-18 12:51:07 +0530

    Add model file for User, Profile and Job

---
+++ model/model.go
@@ -0,0 +1,33 @@
+package model
+
+import "time"
+
+type User struct {
+   Name            string `json:"name"`
+   Email           string `json:"email"`
+   Address         string `json:"address"`
+   UserType        string `json:"usertype"`
+   PasswordHash    string `json:"password_hash"`
+   ProfileHeadline string `json:"profile_headline"`
+
+}
+
+type Profile struct {
+   Applicant      User   `json:"applicant"`
+   ResumeFileAddr string `json:"resume_file_address"`
+   Skills         string `json:"skills"`
+   Education      string `json:"education"`
+   Experience     string `json:"experience"`
+   Name           string `json:"name"`
+   Email          string `json:"email"`
+   Phone          string `json:"phone"`
+}
+
+type Job struct {
+   Title             string     `json:"title"`
+   Description       string     `json:"description"`
+   PostedOn          *time.Time `json:"posted_on"`
+   TotalApplications int        `json:"total_applications"`
+   CompanyName       string     `json:"company_name"`
+   PostedBy          User       `json:"posted_by"`
+}
